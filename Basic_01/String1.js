71d3fb3f686bf93bdd77108b504697427226b3fe
Ravindra Choudhary <84620599+ravindrasirvi609@users.noreply.github.com>
2023-05-19 18:04:09 +0000

    number and dates

--- Basic_01/String1.js
+++ Basic_01/String1.js
@@ -1,23 +1,31 @@
-const name = "ravindra"
+// Type - 1 
+const name1 = "ravindra"
+// Type - 2  
+const name2 = new String('ravindra');
+
+console.log(name1.charAt(2));
+console.log(name1.charCodeAt());
+
+
 const repocount = 50;
 
 console.log(name+ repocount + " value");
 console.log(` my name is ${name} and total repo is ${repocount}`);
 
 
-const newString = new String('ravi-the-cool-boy');
-console.log(newString[0]);
-console.log(newString.__proto__);
-console.log(newString.length);
-console.log(newString.toUpperCase());
-console.log(newString.charAt(2));
-console.log(newString.indexOf("v"));
+//const newString = new String('ravi-the-cool-boy');
+console.log(newString2[0]);
+console.log(newString2.__proto__);
+console.log(newString2.length);
+console.log(newString2.toUpperCase());
+console.log(newString2.charAt(2));
+console.log(newString2.indexOf("v"));
 
 
-const newString2 = newString.substring(0,4)
+const newString2 = newString2.substring(0,4)
 console.log(newString2);
 
-const anotherString = newString.slice(-2,4)
+const anotherString = newString2.slice(-2,4)
 console.log(anotherString);
 
 
@@ -33,4 +41,4 @@ console.log(url.replace('%20',"-"));
 console.log(url.includes('ravindra'));
 console.log(url.includes('vinita'));
 
-console.log(newString.split("-"));
+console.log(newString2.split("-"));
