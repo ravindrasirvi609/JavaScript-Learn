b9548c0d0aeef5a4b9d9d111e770d6c7e001ed18
Ravindra Choudhary <84620599+ravindrasirvi609@users.noreply.github.com>
2023-08-21 18:26:52 +0000

    Color change

---
+++ 07Project/project1.js
@@ -0,0 +1,16 @@
+// Get references to the buttons
+const redButton = document.getElementById('red');
+const greyButton = document.getElementById('grey');
+const pinkButton = document.getElementById('pink');
+const yellowButton = document.getElementById('yellow');
+
+// Add click event listeners to each button
+redButton.addEventListener('click', () => changeBackgroundColor('red'));
+greyButton.addEventListener('click', () => changeBackgroundColor('grey'));
+pinkButton.addEventListener('click', () => changeBackgroundColor('pink'));
+yellowButton.addEventListener('click', () => changeBackgroundColor('yellow'));
+
+// Function to change the background color of the body
+function changeBackgroundColor(color) {
+    document.body.style.backgroundColor = color;
+}
